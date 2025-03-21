a93d74ef5524d62f0910fd7581d75b6b65be9c2d
HelenRaven <HelenRaven@mail.ru>
2025-03-21 21:29:30 +0700

    Repository implemented, refactored

--- src/main/java/org/example/controller/PostController.java
+++ src/main/java/org/example/controller/PostController.java
@@ -17,25 +17,30 @@ public class PostController {
     }
 
     public void all(HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
         final var data = service.all();
-        final var gson = new Gson();
-        response.getWriter().print(gson.toJson(data));
+        sendResponse(data, response);
     }
 
-    public void getById(long id, HttpServletResponse response) {
-        // TODO: deserialize request & serialize response
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        final var data = service.getById(id);
+        sendResponse(data, response);
     }
 
     public void save(Reader body, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
         final var gson = new Gson();
         final var post = gson.fromJson(body, Post.class);
         final var data = service.save(post);
-        response.getWriter().print(gson.toJson(data));
+        sendResponse(data, response);
     }
 
-    public void removeById(long id, HttpServletResponse response) {
-        // TODO: deserialize request & serialize response
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        service.removeById(id);
+        sendResponse("ok", response);
+    }
+
+    private void sendResponse(Object data, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(data));
     }
 }
--- src/main/java/org/example/repository/PostRepository.java
+++ src/main/java/org/example/repository/PostRepository.java
@@ -1,25 +1,48 @@
 package org.example.repository;
 
+import org.example.exception.NotFoundException;
 import org.example.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
 public class PostRepository {
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong counter = new AtomicLong(1);
+
     public List<Post> all() {
-        return Collections.emptyList();
+        return new ArrayList<>(posts.values());
     }
 
     public Optional<Post> getById(long id) {
-        return Optional.empty();
+        try {
+            return Optional.ofNullable(posts.get((int) id));
+        } catch (IndexOutOfBoundsException ignored) {
+            return Optional.empty();
+        }
     }
 
     public Post save(Post post) {
+        if (post.getId() == 0 || !posts.containsKey((int) post.getId())) {
+            create(post);
+        } else {
+            posts.put((int) post.getId(), post);
+        }
         return post;
     }
 
     public void removeById(long id) {
+        try {
+            posts.remove((int) id);
+        } catch (UnsupportedOperationException | ClassCastException | NullPointerException ignored) {
+            throw new NotFoundException();
+        }
+    }
+
+    private void create (Post post){
+        long index = counter.getAndIncrement();
+        post.setId(index);
+        posts.put((int) index, post);
     }
 }
